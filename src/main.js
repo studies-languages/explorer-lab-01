930fee1774ff6b4308232669bed48f00d2c92d24
Lucas de Sousa Assunção <lucasdesousa19@hotmail.com>
2022-10-19 03:51:11 +0000

    aula 1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
 import "./css/index.css"
+
+const ccBgcolor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgcolor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#436d99", "#2d57f2"],
+    mastercard: ["#df6f29", "#c69347"],
+    default: ["black", "gray"],
+  }
+
+  ccBgcolor01.setAttribute("fill", colors[type][0])
+  ccBgcolor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute('src', `cc-${type}.svg`)
+}
+
+setCardType("mastercard")
+
+globalThis.setCardType = setCardType
